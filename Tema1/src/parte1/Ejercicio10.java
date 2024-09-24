ad7a9ac06d1adf7989b0db88bd136f3668bf4367
iziva <iziva@DESKTOP-JERP5OE>
2024-09-24 18:04:33 +0200

    Ejercicio 10 anotado (se me pasó)

--- Tema1/src/parte1/Ejercicio10.java
+++ Tema1/src/parte1/Ejercicio10.java
@@ -12,20 +12,28 @@ public class Ejercicio10 {
 		// Creamos la variable num donde guardaremos el número
 		int num;
 		
+		// Creamos la variable resto
 		int resto;
 		
+		// Creamos la variable boolean par
 		boolean par;
 		
+		// Le pedimos al usuario que introduzca un número
 		System.out.println("Introduzca un número para comprobar si es par");
 		
+		// Introducimos el número recibido en la variable
 		num = lectura.nextInt();
 		
+		// Dividimos el número entre 2 y guardamos el resto en la variable
 		resto = num % 2;
 		
+		// Verificamos si es par o no, para ser par el resto debe ser 0, si no, es impar
 		par = resto == 0;
 		
+		// Le mostramos al usuario el resultado
 		System.out.println(par);
 		
+		// Cerramos el proceso de lectura
 		lectura.close();
 		
 	}
